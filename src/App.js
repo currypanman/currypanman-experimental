c58614f7420fa28f6dbc80f9fdc401c5278ad802
Currypanman <currypanman.spicy@outlook.com>
2023-03-22 22:37:46 +0900

    Add a form to capture images.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const state = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    state ? (
+      <div>
+        <input accept='image/*' capture='environment' className='visually-hidden' type='file' />
+      </div>
+    ) : (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
   );
 }
 
